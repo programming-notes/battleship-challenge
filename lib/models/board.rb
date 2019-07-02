578867d2a13000b75d902f3b72d5a22600f0af5a
= <=>
2019-07-02 00:21:11 -0400

    Adds TTY:Table to board

--- lib/models/board.rb
+++ lib/models/board.rb
@@ -1,7 +1,16 @@
 class Board
   attr_accessor :ships
-  
+
   def initialize
     @coordinates = Array.new(10) { Array.new(10) }
+    @table = TTY::Table.new (0..9), @coordinates
+  end
+
+  def friendly_view
+    
+  end
+
+  def enemy_view
+    table.render
   end
 end
