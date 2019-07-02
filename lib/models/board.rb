9c014dc540ec33579e3df93a0a4d3549da6a0ecb
= <=>
2019-07-02 01:26:27 -0400

    Adds board

--- lib/models/board.rb
+++ lib/models/board.rb
@@ -1,14 +1,8 @@
 class Board
-  attr_accessor :ships
-
   def initialize
     x = 96
     @coordinates = Array.new(10) { [(x+=1).chr].concat(Array.new(10)) }
     @table = TTY::Table.new [' ', *(0..9)], @coordinates
-  end
-
-  def friendly_view
-    
   end
 
   def enemy_view
