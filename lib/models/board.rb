90710b59710ea44b41509fbc2cabeed82fedfaf3
= <=>
2019-07-02 01:17:33 -0400

    Adds board view

--- lib/models/board.rb
+++ lib/models/board.rb
@@ -2,8 +2,9 @@ class Board
   attr_accessor :ships
 
   def initialize
-    @coordinates = Array.new(10) { Array.new(10) }
-    @table = TTY::Table.new (0..9), @coordinates
+    x = 96
+    @coordinates = Array.new(10) { [(x+=1).chr].concat(Array.new(10)) }
+    @table = TTY::Table.new [' ', *(0..9)], @coordinates
   end
 
   def friendly_view
@@ -11,6 +12,17 @@ class Board
   end
 
   def enemy_view
-    table.render
+    @table.render :unicode, padding: [0, 1] do |r|
+      r.border do
+        left ' '
+        mid_left ' '
+        bottom_left ' '
+        top ' '
+        top_mid ' '
+        top_left ' '
+        top_right ' '
+      end
+      r.border.separator = :each_row
+    end
   end
 end
